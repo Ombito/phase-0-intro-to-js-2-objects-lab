97950b6056c5da149eae19e0c2efac37b3af0ab7
ombito <alvinombito09@gmail.com>
2023-07-16 22:19:14 +0300

    Complete assignment

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Write your solution in this file!
+const employee = {
+  name: "Alvin",
+  streetAddress:"Mombasa, Kenya"
+}
+
+function updateEmployeeWithKeyAndValue(employee, name, streetAddress){
+  let newEmployee = {...employee};
+  newEmployee[name] = streetAddress;
+
+  return newEmployee
+
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, name, streetAddress){
+  employee[name] = streetAddress;
+
+  return employee
+}
+function deleteFromEmployeeByKey(employee, name){
+  let newEmployee = {...employee};
+  delete newEmployee[name];
+
+  return newEmployee
+
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, name){
+  delete employee[name];
+
+  return employee
+}
